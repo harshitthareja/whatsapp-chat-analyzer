6478db7dc419ee415c74ffdb058551e38d3a7d54
harshitthareja <118681259+harshitthareja@users.noreply.github.com>
2025-02-23 00:17:57 +0530

    Initial commit

--- preprocessor.py
+++ preprocessor.py
@@ -2,26 +2,29 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s*\d{1,2}:\d{2}\s*(?:am|pm)?\s*-\s'
 
     messages = re.split(pattern, data)[1:]
 
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+    df['message_date'] = pd.to_datetime(df['message_date'].str.replace('\u202F', ' '), format='%d/%m/%y, %I:%M %p - ')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
     messages = []
     for message in df['user_message']:
-        entry = re.split(r'([\w\W]+?):\s', message)
-        if entry[1:]:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
             users.append(entry[1])
-            messages.append(entry[2])
+            messages.append(" ".join(entry[2:]))
         else:
             users.append('group_notification')
             messages.append(entry[0])
+
     df['user'] = users
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
